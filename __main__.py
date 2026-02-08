cb97d7b436b5f2e7296ebdee7aeaaebd57c5034a
BogolyubovNSU <i.bogolyubov@g.nsu.ru>
2026-02-08 17:43:41 +0700

    Создан файл __main__.py Добавлен шаблон для работы

---
+++ __main__.py
@@ -0,0 +1,62 @@
+import turtle
+
+
+t = turtle.Turtle()
+turtle.setup(width=1200, height=1000)
+
+
+def triangle(vertices,color,v):
+    pass
+
+
+def square(x,y,a,color,v):
+    pass
+
+
+def rhombus(x,y,a,angle0,angle1,angle2,color,v):
+    pass
+
+
+def parallelogram(x,y,a1,a2,angle,color,v):
+    pass
+
+
+def general_tengram():
+    pass
+
+
+def picture1():
+    pass
+
+
+def picture2():
+    pass
+
+
+def picture3():
+    pass
+
+
+def picture4():
+    pass
+
+
+def picture5():
+    pass
+
+
+def picture6():
+    pass
+
+
+def picture7():
+    pass
+
+
+def picture8():
+    pass
+
+def main():
+    pass
+
+main()
